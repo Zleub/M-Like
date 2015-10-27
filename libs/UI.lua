5c3086913d260302ff0dfe1f7cb40020511f21f8
Zleub <debray.arnaud@gmail.com>
2015-10-27 12:19:13 +0100

    useless file

---
+++ libs/UI.lua
@@ -0,0 +1,21 @@
+--          `--::-.`
+--      ./shddddddddhs+.
+--    :yddddddddddddddddy:
+--  `sdddddddddddddddddddds`
+--  ydddh+sdddddddddy+ydddds  M-Like:UI
+-- /ddddy:oddddddddds:sddddd/ By adebray - adebray
+-- sdddddddddddddddddddddddds
+-- sdddddddddddddddddddddddds Created: 2015-10-27 11:55:09
+-- :ddddddddddhyyddddddddddd: Modified: 2015-10-27 11:57:42
+--  odddddddd/`:-`sdddddddds
+--   +ddddddh`+dh +dddddddo
+--    -sdddddh///sdddddds-
+--      .+ydddddddddhs/.
+--          .-::::-`
+
+local UI = {}
+
+function UI.button()
+end
+
+return UI
